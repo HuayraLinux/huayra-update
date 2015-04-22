79b65a8c0322264bdc1bbb5c38a6f5a7f72b999f
Ignacio Juan Martín Benedetti <tranceway@gmail.com>
2015-04-22 14:32:55 -0300

    Simplificación de About

--- src/lib/about.py
+++ src/lib/about.py
@@ -17,49 +17,18 @@ class AboutDialog(wx.Frame):
         super(AboutDialog, self).__init__(
             parent=parent,
             title=u'Acerca de',
-            style=wx.CLOSE_BOX
         )
 
-        titular = wx.Panel(parent=self)
-        txt = wx.StaticText(parent=titular, label=u'Huayra Update')
-        txt.SetFont(wx.Font(16, wx.DEFAULT, wx.NORMAL, wx.BOLD))
-
-        szr_titular = wx.BoxSizer(wx.HORIZONTAL)
-        szr_titular.Add(txt)
-        titular.SetSizer(szr_titular)
-
-        panel = wx.Panel(parent=self)
-
-        self.btn_close = wx.Button(parent=panel, label=u'Cerrar')
-        self.btn_close.Bind(wx.EVT_BUTTON, self.OnClose)
-
-        szr_buttons = wx.BoxSizer(wx.HORIZONTAL)
-        szr_buttons.Add(self.btn_close, flag=wx.RIGHT, border=2)
-
-        self.szr_panel = wx.BoxSizer(wx.VERTICAL)
-        self.szr_panel.Add(szr_buttons)
-        panel.SetSizer(self.szr_panel)
-
-        # --
-        self.messages = wxHTML(parent=self)
-        self.messages.LoadPage(os.path.join(
-            wx.GetApp().current_path,
+        html = wxHTML(self)
+        html.LoadPage(
+            os.path.join(
+            wx.GetApp().app_path,
             'media',
             'about.html'
         ))
 
-        # --
-
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(titular, flag=wx.LEFT|wx.ALL, border=5)
-        sizer.Add(self.messages, 1, flag=wx.EXPAND)
-        sizer.Add(panel, flag=wx.ALL|wx.CENTER, border=5)
-
-        self.SetSizer(sizer)
-
-        self.SetMinSize((320, 240))
-        self.SetMaxSize((320, 240))
-        self.Center()
+        self.SetMinSize((350, 250))
+        self.SetMaxSize((350, 250))
 
     def OnClose(self, evt):
         self.Close()
